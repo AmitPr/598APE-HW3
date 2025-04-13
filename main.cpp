b47d5ae1924ec4e8c4cb9f5dd68167ca9e5f63d5
amitp4 <amitp4@sp25-cs598a-16.cs.illinois.edu>
2025-04-13 14:14:47 -0500

    Switch to AVX 256

--- main.cpp
+++ main.cpp
@@ -5,6 +5,14 @@
 #include <string.h>
 #include <sys/time.h>
 
+#define CACHE_LOOKAHEAD 64
+
+int nplanets;
+int timesteps;
+const double dt = 0.001;
+const double G = 6.6743;
+unsigned long long seed = 100;
+
 float tdiff(struct timeval* start, struct timeval* end) {
   return (end->tv_sec - start->tv_sec) + 1e-6 * (end->tv_usec - start->tv_usec);
 }
@@ -46,8 +54,6 @@ struct World {
   }
 };
 
-unsigned long long seed = 100;
-
 unsigned long long randomU64() {
   seed ^= (seed << 21);
   seed ^= (seed >> 35);
@@ -63,64 +69,71 @@ double randomDouble() {
   return ((next << 27) + next2) / (double)(1LL << 53);
 }
 
-int nplanets;
-int timesteps;
-const double dt = 0.001;
-const double G = 6.6743;
+#define set1(a) _mm256_set1_pd(a)
+#define zero() _mm256_setzero_pd()
+#define mul(a, b) _mm256_mul_pd(a, b)
+#define add(a, b) _mm256_add_pd(a, b)
+#define sub(a, b) _mm256_sub_pd(a, b)
+#define sqrt(a) _mm256_sqrt_pd(a)
+#define rsqrt(a) _mm256_rsqrt14_pd(a)
+#define div(a, b) _mm256_div_pd(a, b)
+#define load(a) _mm256_load_pd(a)
+#define store(a, b) _mm256_store_pd(a, b)
+#define fmadd(a, b, c) _mm256_fmadd_pd(a, b, c)
+// #define reduce_add(a) _mm512_reduce_add_pd(a)
+#define reduce_add(a) _mm256_reduce_add_pd(a)
 
-#define mul(a, b) _mm512_mul_pd(a, b)
-#define add(a, b) _mm512_add_pd(a, b)
-#define sub(a, b) _mm512_sub_pd(a, b)
-#define sqrt(a) _mm512_sqrt_pd(a)
-#define rsqrt(a) _mm512_rsqrt14_pd(a)
-#define div(a, b) _mm512_div_pd(a, b)
-#define load(a) _mm512_load_pd(a)
-#define store(a, b) _mm512_store_pd(a, b)
-#define set1(a) _mm512_set1_pd(a)
-#define fmadd(a, b, c) _mm512_fmadd_pd(a, b, c)
+inline double _mm256_reduce_add_pd(__m256d v) {
+  // v[:128] + v[128:]
+  __m128d vlow = _mm256_castpd256_pd128(v);
+  __m128d vhigh = _mm256_extractf128_pd(v, 1);
+  vlow = _mm_add_pd(vlow, vhigh);
 
-#define CACHE_LOOKAHEAD 64
+  // vlow[:64] + vlow[64:]
+  __m128d high64 = _mm_unpackhi_pd(vlow, vlow);
+  return _mm_cvtsd_f64(_mm_add_sd(vlow, high64));
+}
 
 void simulate(World& world) {
-  __m512d dt_vec = set1(dt);
-  __m512d epsilon = set1(0.0001);
+  __m256d dt_vec = set1(dt);
+  __m256d epsilon = set1(0.0001);
 #pragma omp parallel for schedule(static)
   for (int i = 0; i < nplanets; i++) {
-    const __m512d x_i = set1(world.x[i]);
-    const __m512d y_i = set1(world.y[i]);
-    const __m512d mass_i = set1(world.mass[i]);
-    __m512d acc_x = _mm512_setzero_pd();
-    __m512d acc_y = _mm512_setzero_pd();
-    for (int j = 0; j < nplanets; j += 8) {
-      const __m512d dx = sub(load(world.x + j), x_i);
-      const __m512d dy = sub(load(world.y + j), y_i);
-      const __m512d distSqr = fmadd(dx, dx, fmadd(dy, dy, epsilon));
+    const __m256d x_i = set1(world.x[i]);
+    const __m256d y_i = set1(world.y[i]);
+    const __m256d mass_i = set1(world.mass[i]);
+    __m256d acc_x = zero();
+    __m256d acc_y = zero();
+    for (int j = 0; j < nplanets; j += 4) {
+      const __m256d dx = sub(load(world.x + j), x_i);
+      const __m256d dy = sub(load(world.y + j), y_i);
+      const __m256d distSqr = fmadd(dx, dx, fmadd(dy, dy, epsilon));
 #ifdef EXACT
-      const __m512d invDist =
+      const __m256d invDist =
           div(mul(mass_i, load(world.mass + j)), sqrt(distSqr));
 #else
-      const __m512d invDist =
+      const __m256d invDist =
           mul(mul(mass_i, load(world.mass + j)), rsqrt(distSqr));
 #endif
-      const __m512d invDist3 = mul(mul(invDist, invDist), invDist);
+      const __m256d invDist3 = mul(mul(invDist, invDist), invDist);
       // acc = acc + dx*invDist3
       acc_x = fmadd(dx, invDist3, acc_x);
       acc_y = fmadd(dy, invDist3, acc_y);
     }
-    world.vx[i] += dt * _mm512_reduce_add_pd(acc_x);
-    world.vy[i] += dt * _mm512_reduce_add_pd(acc_y);
+    world.vx[i] += dt * reduce_add(acc_x);
+    world.vy[i] += dt * reduce_add(acc_y);
   }
 
-  for (int i = 0; i < nplanets; i += 8) {
+  for (int i = 0; i < nplanets; i += 4) {
     // p = p + v*dt
-    const __m512d x = fmadd(dt_vec, load(world.vx + i), load(world.x + i));
-    const __m512d y = fmadd(dt_vec, load(world.vy + i), load(world.y + i));
+    const __m256d x = fmadd(dt_vec, load(world.vx + i), load(world.x + i));
+    const __m256d y = fmadd(dt_vec, load(world.vy + i), load(world.y + i));
     store(world.x + i, x);
     store(world.y + i, y);
 
-    // printf("%0.16f %0.16f ", world.x[i], world.y[i]);
+    printf("%0.16f %0.16f ", world.x[i], world.y[i]);
   }
-  // printf("\n");
+  printf("\n");
 }
 
 int main(int argc, const char** argv) {
