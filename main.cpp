65c7f732a454cefc9dfd9c05f768ab02cb33b000
amitp4 <amitp4@sp25-cs598a-16.cs.illinois.edu>
2025-04-13 14:39:01 -0500

    Conditional compilation between 512 and 256

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <sys/time.h>
 
 #define CACHE_LOOKAHEAD 64
+// #define AVX512
+
+#include "vec.h"
 
 int nplanets;
 int timesteps;
@@ -38,11 +41,11 @@ struct World {
       exit(1);
     }
 
-    memset(mass, 0, sizeof(double) * nplanets);
-    memset(x, 0, sizeof(double) * nplanets);
-    memset(y, 0, sizeof(double) * nplanets);
-    memset(vx, 0, sizeof(double) * nplanets);
-    memset(vy, 0, sizeof(double) * nplanets);
+    memset(mass, 0, size);
+    memset(x, 0, size);
+    memset(y, 0, size);
+    memset(vx, 0, size);
+    memset(vy, 0, size);
   }
 
   ~World() {
@@ -69,53 +72,26 @@ double randomDouble() {
   return ((next << 27) + next2) / (double)(1LL << 53);
 }
 
-#define set1(a) _mm256_set1_pd(a)
-#define zero() _mm256_setzero_pd()
-#define mul(a, b) _mm256_mul_pd(a, b)
-#define add(a, b) _mm256_add_pd(a, b)
-#define sub(a, b) _mm256_sub_pd(a, b)
-#define sqrt(a) _mm256_sqrt_pd(a)
-#define rsqrt(a) _mm256_rsqrt14_pd(a)
-#define div(a, b) _mm256_div_pd(a, b)
-#define load(a) _mm256_load_pd(a)
-#define store(a, b) _mm256_store_pd(a, b)
-#define fmadd(a, b, c) _mm256_fmadd_pd(a, b, c)
-// #define reduce_add(a) _mm512_reduce_add_pd(a)
-#define reduce_add(a) _mm256_reduce_add_pd(a)
-
-inline double _mm256_reduce_add_pd(__m256d v) {
-  // v[:128] + v[128:]
-  __m128d vlow = _mm256_castpd256_pd128(v);
-  __m128d vhigh = _mm256_extractf128_pd(v, 1);
-  vlow = _mm_add_pd(vlow, vhigh);
-
-  // vlow[:64] + vlow[64:]
-  __m128d high64 = _mm_unpackhi_pd(vlow, vlow);
-  return _mm_cvtsd_f64(_mm_add_sd(vlow, high64));
-}
-
 void simulate(World& world) {
-  __m256d dt_vec = set1(dt);
-  __m256d epsilon = set1(0.0001);
+  vec dt_vec = set1(dt);
+  vec epsilon = set1(0.0001);
 #pragma omp parallel for schedule(static)
   for (int i = 0; i < nplanets; i++) {
-    const __m256d x_i = set1(world.x[i]);
-    const __m256d y_i = set1(world.y[i]);
-    const __m256d mass_i = set1(world.mass[i]);
-    __m256d acc_x = zero();
-    __m256d acc_y = zero();
-    for (int j = 0; j < nplanets; j += 4) {
-      const __m256d dx = sub(load(world.x + j), x_i);
-      const __m256d dy = sub(load(world.y + j), y_i);
-      const __m256d distSqr = fmadd(dx, dx, fmadd(dy, dy, epsilon));
+    const vec x_i = set1(world.x[i]);
+    const vec y_i = set1(world.y[i]);
+    const vec mass_i = set1(world.mass[i]);
+    vec acc_x = zero();
+    vec acc_y = zero();
+    for (int j = 0; j < nplanets; j += WIDTH) {
+      const vec dx = sub(load(world.x + j), x_i);
+      const vec dy = sub(load(world.y + j), y_i);
+      const vec distSqr = fmadd(dx, dx, fmadd(dy, dy, epsilon));
 #ifdef EXACT
-      const __m256d invDist =
-          div(mul(mass_i, load(world.mass + j)), sqrt(distSqr));
+      const vec inv = div(mul(mass_i, load(world.mass + j)), sqrt(distSqr));
 #else
-      const __m256d invDist =
-          mul(mul(mass_i, load(world.mass + j)), rsqrt(distSqr));
+      const vec inv = mul(mul(mass_i, load(world.mass + j)), rsqrt(distSqr));
 #endif
-      const __m256d invDist3 = mul(mul(invDist, invDist), invDist);
+      const vec invDist3 = mul(mul(inv, inv), inv);
       // acc = acc + dx*invDist3
       acc_x = fmadd(dx, invDist3, acc_x);
       acc_y = fmadd(dy, invDist3, acc_y);
@@ -124,16 +100,16 @@ void simulate(World& world) {
     world.vy[i] += dt * reduce_add(acc_y);
   }
 
-  for (int i = 0; i < nplanets; i += 4) {
+  for (int i = 0; i < nplanets; i += WIDTH) {
     // p = p + v*dt
-    const __m256d x = fmadd(dt_vec, load(world.vx + i), load(world.x + i));
-    const __m256d y = fmadd(dt_vec, load(world.vy + i), load(world.y + i));
+    const vec x = fmadd(dt_vec, load(world.vx + i), load(world.x + i));
+    const vec y = fmadd(dt_vec, load(world.vy + i), load(world.y + i));
     store(world.x + i, x);
     store(world.y + i, y);
-
-    printf("%0.16f %0.16f ", world.x[i], world.y[i]);
   }
-  printf("\n");
+  // for (int i = 0; i < nplanets; i+=16)
+  //   printf("%0.16f %0.16f ", world.x[i], world.y[i]);
+  // printf("\n");
 }
 
 int main(int argc, const char** argv) {
@@ -151,7 +127,9 @@ int main(int argc, const char** argv) {
     world.y[i] = (randomDouble() - 0.5) * 100 * pow(1 + nplanets, 0.4);
     world.vx[i] = randomDouble() * 5 - 2.5;
     world.vy[i] = randomDouble() * 5 - 2.5;
+    // if (i%16 ==0) printf("%0.16f %0.16f ", world.x[i], world.y[i]);
   }
+  // printf("\n");
 
   struct timeval start, end;
   gettimeofday(&start, NULL);
---
+++ vec.h
@@ -0,0 +1,47 @@
+#ifndef VEC_H
+#define VEC_H
+#include <immintrin.h>
+#ifdef AVX512
+#define set1(a) _mm512_set1_pd(a)
+#define zero() _mm512_setzero_pd()
+#define mul(a, b) _mm512_mul_pd(a, b)
+#define add(a, b) _mm512_add_pd(a, b)
+#define sub(a, b) _mm512_sub_pd(a, b)
+#define sqrt(a) _mm512_sqrt_pd(a)
+#define rsqrt(a) _mm512_rsqrt14_pd(a)
+#define div(a, b) _mm512_div_pd(a, b)
+#define load(a) _mm512_load_pd(a)
+#define store(a, b) _mm512_store_pd(a, b)
+#define fmadd(a, b, c) _mm512_fmadd_pd(a, b, c)
+#define reduce_add(a) _mm512_reduce_add_pd(a)
+typedef __m512d vec;
+#define WIDTH 8
+#else
+#define set1(a) _mm256_set1_pd(a)
+#define zero() _mm256_setzero_pd()
+#define mul(a, b) _mm256_mul_pd(a, b)
+#define add(a, b) _mm256_add_pd(a, b)
+#define sub(a, b) _mm256_sub_pd(a, b)
+#define sqrt(a) _mm256_sqrt_pd(a)
+#define rsqrt(a) _mm256_rsqrt14_pd(a)
+#define div(a, b) _mm256_div_pd(a, b)
+#define load(a) _mm256_load_pd(a)
+#define store(a, b) _mm256_store_pd(a, b)
+#define fmadd(a, b, c) _mm256_fmadd_pd(a, b, c)
+#define reduce_add(a) _mm256_reduce_add_pd(a)
+typedef __m256d vec;
+#define WIDTH 4
+
+inline double _mm256_reduce_add_pd(vec v) {
+  // v[:128] + v[128:]
+  __m128d vlow = _mm256_castpd256_pd128(v);
+  __m128d vhigh = _mm256_extractf128_pd(v, 1);
+  vlow = _mm_add_pd(vlow, vhigh);
+
+  // vlow[:64] + vlow[64:]
+  __m128d high64 = _mm_unpackhi_pd(vlow, vlow);
+  return _mm_cvtsd_f64(_mm_add_sd(vlow, high64));
+}
+#endif
+
+#endif  // VEC_H
