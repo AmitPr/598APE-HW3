48fa1b64410cda2089fd9c94b0e5f44f6304fcc4
amitp4 <amitp4@sp25-cs598a-16.cs.illinois.edu>
2025-04-12 20:41:34 -0500

    SoA

--- main.cpp
+++ main.cpp
@@ -7,12 +7,20 @@ float tdiff(struct timeval* start, struct timeval* end) {
   return (end->tv_sec - start->tv_sec) + 1e-6 * (end->tv_usec - start->tv_usec);
 }
 
-struct Planet {
-  double mass;
-  double x;
-  double y;
-  double vx;
-  double vy;
+struct World {
+  double* mass;
+  double* x;
+  double* y;
+  double* vx;
+  double* vy;
+
+  World(int n) {
+    mass = (double*)malloc(sizeof(double) * n);
+    x = (double*)malloc(sizeof(double) * n);
+    y = (double*)malloc(sizeof(double) * n);
+    vx = (double*)malloc(sizeof(double) * n);
+    vy = (double*)malloc(sizeof(double) * n);
+  }
 };
 
 unsigned long long seed = 100;
@@ -37,31 +45,22 @@ int timesteps;
 double dt;
 double G;
 
-Planet* next(Planet* planets) {
-  Planet* nextplanets = (Planet*)malloc(sizeof(Planet) * nplanets);
-  for (int i = 0; i < nplanets; i++) {
-    nextplanets[i].vx = planets[i].vx;
-    nextplanets[i].vy = planets[i].vy;
-    nextplanets[i].mass = planets[i].mass;
-    nextplanets[i].x = planets[i].x;
-    nextplanets[i].y = planets[i].y;
-  }
-
-  for (int i = 0; i < nplanets; i++) {
-    for (int j = 0; j < nplanets; j++) {
-      double dx = planets[j].x - planets[i].x;
-      double dy = planets[j].y - planets[i].y;
-      double distSqr = dx * dx + dy * dy + 0.0001;
-      double invDist = planets[i].mass * planets[j].mass / sqrt(distSqr);
-      double invDist3 = invDist * invDist * invDist;
-      nextplanets[i].vx += dt * dx * invDist3;
-      nextplanets[i].vy += dt * dy * invDist3;
+void simulate(World& world) {
+  for (int t = 0; t < timesteps; t++) {
+    for (int i = 0; i < nplanets; i++) {
+      for (int j = 0; j < nplanets; j++) {
+        double dx = world.x[j] - world.x[i];
+        double dy = world.y[j] - world.y[i];
+        double distSqr = dx * dx + dy * dy + 0.0001;
+        double invDist = world.mass[i] * world.mass[j] / sqrt(distSqr);
+        double invDist3 = invDist * invDist * invDist;
+        world.vx[i] += dt * dx * invDist3;
+        world.vy[i] += dt * dy * invDist3;
+      }
+      world.x[i] += dt * world.vx[i];
+      world.y[i] += dt * world.vy[i];
     }
-    nextplanets[i].x += dt * nextplanets[i].vx;
-    nextplanets[i].y += dt * nextplanets[i].vy;
   }
-  free(planets);
-  return nextplanets;
 }
 
 int main(int argc, const char** argv) {
@@ -74,25 +73,21 @@ int main(int argc, const char** argv) {
   dt = 0.001;
   G = 6.6743;
 
-  Planet* planets = (Planet*)malloc(sizeof(Planet) * nplanets);
+  World world(nplanets);
   for (int i = 0; i < nplanets; i++) {
-    planets[i].mass = randomDouble() * 10 + 0.2;
-    planets[i].x = (randomDouble() - 0.5) * 100 * pow(1 + nplanets, 0.4);
-    planets[i].y = (randomDouble() - 0.5) * 100 * pow(1 + nplanets, 0.4);
-    planets[i].vx = randomDouble() * 5 - 2.5;
-    planets[i].vy = randomDouble() * 5 - 2.5;
+    world.mass[i] = randomDouble() * 10 + 0.2;
+    world.x[i] = (randomDouble() - 0.5) * 100 * pow(1 + nplanets, 0.4);
+    world.y[i] = (randomDouble() - 0.5) * 100 * pow(1 + nplanets, 0.4);
+    world.vx[i] = randomDouble() * 5 - 2.5;
+    world.vy[i] = randomDouble() * 5 - 2.5;
   }
 
   struct timeval start, end;
   gettimeofday(&start, NULL);
-  for (int i = 0; i < timesteps; i++) {
-    planets = next(planets);
-    // printf("x=%f y=%f vx=%f vy=%f\n", planets[nplanets-1].x,
-    // planets[nplanets-1].y, planets[nplanets-1].vx, planets[nplanets-1].vy);
-  }
+  simulate(world);
   gettimeofday(&end, NULL);
   printf("Total time to run simulation %0.6f seconds, final location %f %f\n",
-         tdiff(&start, &end), planets[nplanets - 1].x, planets[nplanets - 1].y);
+         tdiff(&start, &end), world.x[nplanets - 1], world.y[nplanets - 1]);
 
   return 0;
 }
